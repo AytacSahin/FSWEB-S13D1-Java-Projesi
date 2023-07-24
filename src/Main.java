bffffcbd7fcc35bacc834ba59afa1ae320a20301
AytacSahin <haytacsahin@gmail.com>
2023-07-24 15:15:02 +0300

    all tasks finished

--- src/Main.java
+++ src/Main.java
@@ -18,47 +18,33 @@ public class Main {
 //        System.out.println(area(5.0, 4.0));
 //        System.out.println(area(-1.0, 4.0));
 
-        System.out.println(area(5.0));
+//        System.out.println(area(5.0));
 
     }
 
     ;
 
     public static boolean shouldWakeUp(boolean mode, int inTime) {
-        if (mode == true && (inTime > 20 || inTime < 8) && (inTime >= 0 && inTime < 24)) {
-            return true;
-        } else {
-            return false;
-        }
+        return ((mode && (inTime > 20 || inTime < 8) && (inTime >= 0 && inTime < 24)));
     }
 
     public static boolean hasTeen(int num1, int num2, int num3) {
-        if (num1 > 13 && num1 <= 19 || num2 > 13 && num2 <= 19 || num3 > 13 && num3 <= 19) {
-            return true;
-        } else {
-            return false;
-        }
+        return (num1 > 13 && num1 <= 19 || num2 > 13 && num2 <= 19 || num3 > 13 && num3 <= 19);
     }
 
     ;
 
     public static boolean isCatPlaying(boolean mode, int degree) {
-        if (mode && 25 <= degree && degree <= 45) {
-            return true;
-        } else if (!mode && 25 <= degree && degree <= 35) {
-            return true;
-        } else {
-            return false;
-        }
+        return ((mode && 25 <= degree && degree <= 45) || (!mode && 25 <= degree && degree <= 35));
     }
 
     ;
 
-//    public static double area(double a, double b) {
-//        return (a < 0 || b < 0) ? -1 : (a * b);
-//    }
-//
-//    ;
+    public static double area(double a, double b) {
+        return (a < 0 || b < 0) ? -1 : (a * b);
+    }
+
+    ;
 
     public static double area(double radius) {
         return (radius < 0) ? -1 : (radius * radius * Math.PI);
